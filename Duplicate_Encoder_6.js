0aba665703b32faa4055732beb03f6ae0a02b854
MordecaiO <cai.oladimeji@gmail.com>
2022-07-29 09:00:27 +0100

    creating plan and half of logic

--- Duplicate_Encoder_6.js
+++ Duplicate_Encoder_6.js
@@ -8,3 +8,30 @@ Examples
 "Success"  =>  ")())())"
 "(( @"     =>  "))((" 
 */
+
+// take string and count the instances of each indv char
+// use object to store values
+  // split into arr > for each loop with filter method on array > length of filtered arr = count
+// take string and evaluate each char using object properties
+// if char has a count > 1 remove and replace with corresponding bracket
+  // array/obj notation and splice to replace 
+// return string of brackets 
+
+
+function duplicateEncode(word){
+   let countObj = {}; 
+  let charsArr = word.split(''); 
+  console.log('charsArr', charsArr); 
+  
+  charsArr.forEach((element, index, arr) => { 
+    if (countObj[element]){
+      countObjj[element]++
+    } else { countObj[element] = 1;}
+    
+    console.log('countObj', countObj);
+    console.log('charsArr', charsArr);
+  })
+  
+  
+ return charsArr.join('')
+}
