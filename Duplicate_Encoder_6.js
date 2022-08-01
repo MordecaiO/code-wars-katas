c46a3ff6fdeef8499ccd80bed64cba88ad9caf2d
MordecaiO <cai.oladimeji@gmail.com>
2022-08-01 07:43:12 +0100

    completed

--- Duplicate_Encoder_6.js
+++ Duplicate_Encoder_6.js
@@ -1,17 +1,6 @@
-// The goal of this exercise is to convert a string to a new string where each character in the new string 
-// is "(" if that character appears only once in the original string, or ")" 
-// if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
-/*
-Examples
-"din"      =>  "((("
-"recede"   =>  "()()()"
-"Success"  =>  ")())())"
-"(( @"     =>  "))((" 
-*/
-
 // take string and count the instances of each indv char
 // use object to store values
-  // split into arr > for each loop with filter method on array > length of filtered arr = count
+  // split into arr > for each loop if char is in object add one if not create new property thats equal to one
 // take string and evaluate each char using object properties
 // if char has a count > 1 remove and replace with corresponding bracket
   // array/obj notation and splice to replace 
@@ -20,18 +9,22 @@ Examples
 
 function duplicateEncode(word){
    let countObj = {}; 
-  let charsArr = word.split(''); 
+  let rtrArr = [];
+  let charsArr = word.toLowerCase().split(''); 
   console.log('charsArr', charsArr); 
   
   charsArr.forEach((element, index, arr) => { 
     if (countObj[element]){
-      countObjj[element]++
+      countObj[element]++
     } else { countObj[element] = 1;}
     
     console.log('countObj', countObj);
     console.log('charsArr', charsArr);
   })
   
+  charsArr.forEach(element => {
+    (countObj[element] === 1) ? rtrArr.push("(") : rtrArr.push(")")
+    })
   
- return charsArr.join('')
+ return rtrArr.join('')
 }
