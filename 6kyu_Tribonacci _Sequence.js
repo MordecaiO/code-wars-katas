97cf4fa19985b3163a6064096a67aad45be3a0c9
MordecaiO <cai.oladimeji@gmail.com>
2022-08-15 08:16:57 +0100

    Create 6kyu_Tribonacci _Sequence.js

---
+++ 6kyu_Tribonacci _Sequence.js
@@ -0,0 +1,27 @@
+/* Plan
+function must take sum of last three items and add to sequence 
+n = number of items 
+n -3 = amount of times a new number is added 
+define for loop (must be lesser than n -3 )
+define var for sum 
+sum first 3 items from the end of the array 
+push this value into the array, clear sum variable
+create unique cases for when n < 3
+return arr 
+*/
+
+
+function tribonacci(signature,n){
+  
+  for (let i = 3; i < n; i++){
+  let sum = signature[signature.length-1] + signature[signature.length-2] + signature[signature.length-3]; 
+    signature.push(sum);
+  }
+if (n === 1){
+  return [signature[0]];
+} else if (n === 2) {
+  return  [signature[0], signature[1]]; 
+} else if (n === 0) {
+  return []
+} else { return signature; }
+}
