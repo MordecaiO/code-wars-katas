0d16a2c2c993b3471e73ce555fc7a2f25d0d8210
MordecaiO <cai.oladimeji@gmail.com>
2022-08-22 09:37:46 +0100

    Update 6kyu_Convert_a_string_to_camel_case.js

--- 6kyu/6kyu_Convert_a_string_to_camel_case.js
+++ 6kyu/6kyu_Convert_a_string_to_camel_case.js
@@ -5,3 +5,33 @@ Examples
 "the-stealth-warrior" gets converted to "theStealthWarrior"
 "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
 */
+
+
+/* Plan
+- determine which delimiter is being used in str 
+- create function which.......
+- take str and split words by delimiter 
+- change the first char of each word to uppercase (except first word which stays the same always)
+- join the words back together with no delimeter
+- depending on which delimiter, apply function respectively
+*/
+
+
+
+function toCamelCase(str){
+
+  let camelCase = (str, delimiter) => {
+    
+    let arrWords = str.split(delimiter);
+    
+   const arrCamelCaseWords = arrWords.map((word, index) => {
+     let firstLetter = word.charAt(0); 
+      if(index > 0){
+        return word.replace(firstLetter, firstLetter.toUpperCase());
+      } else { return word }
+    })
+    return arrCamelCaseWords.join('');
+  }
+  
+return str.includes("-") ? camelCase(str, "-") : camelCase(str, "_");
+}
