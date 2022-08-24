9cdf6ab01061b7925acd8460758da2bdfcf0a955
MordecaiO <cai.oladimeji@gmail.com>
2022-08-24 23:53:08 +0100

    Update 5kyu_Where_my_anagrams_at.js

--- 5kyu/5kyu_Where_my_anagrams_at.js
+++ 5kyu/5kyu_Where_my_anagrams_at.js
@@ -16,3 +16,52 @@ anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', '
 
 anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
 */
+/* given two inputs a word and an array with words
+    return an array of all the anagrams 
+    empty array if there are none
+*/
+
+/* PLAN
+comparing two strings - do they posses the same characters the same amount of times
+function which counts num of each char in word - returns number 
+funtion which returns true if a char is present in both words
+use functions on every char in given single word 
+if word passes for every single char push to anagrams array
+
+*/
+function anagrams(word, words) {
+  let anagrams = []; 
+  
+  // counts how many times char occurs in string
+  const countChar = (string, char) => {
+    let count = string.split(char).length - 1; 
+    return count; 
+  }
+  
+  
+  // returns true if char is present in both strings
+  const presentInBoth = (str1, str2, char) => {
+    return str1.includes(char) && str2.includes(char) ? true : false;
+  }
+  
+  
+ words.forEach((item) => {
+ 
+   
+   let status = 0; 
+   console.log('status', status); 
+   
+   for (let i in item) {
+     
+     let currentChar = item[i]
+     
+     if ( countChar(item, currentChar) !== countChar(word, currentChar) || presentInBoth(item, word, currentChar) !== true) { 
+     status++
+     } 
+   } 
+    if (status === 0) {
+     anagrams.push(item); 
+   }
+})
+  return anagrams ; 
+}
