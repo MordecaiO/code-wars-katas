aceabec9e3454dff56b8d276c1b66c20354a9096
MordecaiO <cai.oladimeji@gmail.com>
2022-09-04 10:29:47 +0100

    Update 6kyu_Does_my_number_look_big_in_this.js

--- 6kyu/6kyu_Does_my_number_look_big_in_this.js
+++ 6kyu/6kyu_Does_my_number_look_big_in_this.js
@@ -15,3 +15,28 @@ This may be True and False in your language, e.g. PHP.
 
 Error checking for text strings or other invalid inputs is not required, only valid positive non-zero integers will be passed into the function.
 */
+// sum of each digit raised to the power of how many digits there are = the number
+/* Plan
+- take value change into string then split into array
+- length of array equals the #digits
+- create sum var 
+- loop through array 
+- turn each item into int then square and add to sum 
+- return true if sum strict equals value false if not
+*/
+
+function narcissistic(value) {
+  
+  let sum = 0; 
+  let arrValue = value.toString().split(''); 
+  console.log('arrValue', arrValue); 
+  
+  let numDigits = arrValue.length;
+  console.log('numDigits', numDigits); 
+  
+  arrValue.forEach((digit) => {
+    sum += Math.pow(Number(digit), numDigits);
+    console.log('sum', sum); 
+  })
+  
+  return value === sum ? true : false ; 
