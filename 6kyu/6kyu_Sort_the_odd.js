7d2a2aea4c5281bbe781ed2e940afa73bb3ef662
MordecaiO <cai.oladimeji@gmail.com>
2022-09-14 21:13:02 +0100

    Update 6kyu_Sort_the_odd.js

--- 6kyu/6kyu_Sort_the_odd.js
+++ 6kyu/6kyu_Sort_the_odd.js
@@ -7,3 +7,30 @@ Examples
 [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
 [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 */
+
+/* Plan 
+- extract odd numbers into new array 
+- sort odd numbers into ascending order 
+- create counter/iterator var for odd # array
+- loop through original array 
+- if word is odd 
+- replace with current item from odd # array determined by iterator 
+- add one to iterator 
+*/
+
+function sortArray(array) {
+  
+ let orderedOddNumbers = array.filter((x) => {return x % 2 !== 0}).sort((a,b) => {return a - b});
+  console.log('filteredOddNumbers', orderedOddNumbers);
+  
+  let k = 0; 
+  
+  array.forEach((element, index) => {
+    if(element % 2 !== 0){
+      array[index] = orderedOddNumbers[k]; 
+      k++
+    }
+    console.log('array', array);
+  });
+  return array; 
+}
