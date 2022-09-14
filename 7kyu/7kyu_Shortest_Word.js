3d4094d84dc9a1179ca099c1bc62e9e727c2637c
MordecaiO <cai.oladimeji@gmail.com>
2022-09-14 11:08:28 +0100

    Update 7kyu_Shortest_Word.js

--- 7kyu/7kyu_Shortest_Word.js
+++ 7kyu/7kyu_Shortest_Word.js
@@ -3,3 +3,32 @@ Simple, given a string of words, return the length of the shortest word(s).
 
 String will never be empty and you do not need to account for different data types.
 */
+
+/* Plan
+- create return var (shortest word) 
+- create arr 
+- initially set return var to length of first item in array
+- loop through arr
+- if current item length is < return var
+- set return var to current item 
+*/
+
+
+function findShort(s){
+  
+  let arrWords = s.split(' ');
+  console.log('arrWords', arrWords);
+  
+  let lengthShortestWord = arrWords[0].length;
+  
+  for(let i = 0; i < arrWords.length ; i++){
+    
+    console.log('arrWords[i]', arrWords[i]);
+    
+    if(arrWords[i].length < lengthShortestWord){
+      lengthShortestWord = arrWords[i].length;
+    }
+    console.log('lengthShortestWord', lengthShortestWord);
+  }
+  return lengthShortestWord;
+}
