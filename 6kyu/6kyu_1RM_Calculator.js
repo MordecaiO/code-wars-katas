06ca99a25b8271118f2039b3663873ed0988e461
MordecaiO <cai.oladimeji@gmail.com>
2022-09-19 10:34:24 +0100

    Update 6kyu_1RM_Calculator.js

--- 6kyu/6kyu_1RM_Calculator.js
+++ 6kyu/6kyu_1RM_Calculator.js
@@ -15,3 +15,35 @@ Since you are not sure which formula to use and you are feeling confident, your 
 rounded to the nearest integer. However, if the number of repetitions passed in is 1 (i.e., it is already a one rep max), your function must return w. 
 Also, if the number of repetitions passed in is 0 (i.e., no repetitions were completed), your function must return 0.
 */
+
+
+//cases = reps > 1, reps = 1 & reps = 1 
+/*Plan
+- if the reps are greater than 1 
+- compute w & r using formulas 
+- take maximum and return it 
+- if reps = 1 
+- use computed 1RM to find w 
+- return w 
+*/
+
+
+function calculate1RM(w, r){
+  
+  let oneRepMax = 0; 
+  
+  let epley = w * (1 + (r/30)); 
+  let mcglothin = (100 * w)/(101.3 - (2.67123 * r)); 
+  let lombardi = Math.pow( r, 0.10) * w; 
+  
+  let largestValue = Math.max(Math.round(epley), Math.round(mcglothin), Math.round(lombardi) ); 
+  
+  
+         if (r > 1){
+           oneRepMax = largestValue; 
+         } else if (r === 1){
+           oneRepMax = w; 
+         }                                      
+  
+  return oneRepMax
+}
