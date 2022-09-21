3503832a87c33f2c654f9744b8a3c5d5f6f201cc
MordecaiO <cai.oladimeji@gmail.com>
2022-09-21 21:14:13 +0100

    Update 5kyu_Rot13.js

--- 5kyu/5kyu_Rot13.js
+++ 5kyu/5kyu_Rot13.js
@@ -8,3 +8,39 @@ Only letters from the latin/english alphabet should be shifted, like in the orig
 
 */
 
+/* plan 
+- create key using input and output corresponding strings 
+- create return arr 
+- split message into array 
+- loop through array 
+- if item is present in input 
+- find index of item in input 
+- use index to push item in output to return arr
+- if item not present in input push item to return array 
+- join return array and return 
+
+*/
+
+function rot13(message){
+  let ciphered = []; 
+  const input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  const output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
+  
+  let messageArr = message.split(''); 
+ 
+  messageArr.forEach((letter) =>{
+    
+
+    if(input.includes(letter)){
+       
+       const i = input.indexOf(letter);
+    ciphered.push(output[i]); 
+       } else {
+    ciphered.push(letter); 
+  }
+  });
+  
+let decoded =  ciphered.join('');
+  
+return decoded;
+  }
