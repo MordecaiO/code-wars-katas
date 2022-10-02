9ff1b68f5410464c754acf37b4ca3fe49654c891
MordecaiO <cai.oladimeji@gmail.com>
2022-10-02 12:32:33 +0100

    Update 6kyu_Is_a_number_prime.js

--- 6kyu/6kyu_Is_a_number_prime.js
+++ 6kyu/6kyu_Is_a_number_prime.js
@@ -14,3 +14,23 @@ is_prime(2)  /* true  */
 is_prime(-1) /* false */
 */
 
+function isPrime(num) {
+  
+  
+  if (num <= 1){
+    return false; 
+    
+  } else if (num === 2 || num === 3 ) {
+    return true
+    
+  }
+  else {
+    
+    for (let i = 2; i <= Math.sqrt(num); i++){
+      if(num % i === 0){
+        return false
+      }
+    }
+  }
+  return true ; 
+}
