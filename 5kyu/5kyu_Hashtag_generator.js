7ca4f3c78487084e2dda05b878da765532eef6e2
MordecaiO <cai.oladimeji@gmail.com>
2022-10-08 12:05:01 +0100

    Update 5kyu_Hashtag_generator.js

--- 5kyu/5kyu_Hashtag_generator.js
+++ 5kyu/5kyu_Hashtag_generator.js
@@ -13,3 +13,52 @@ Examples
 "    Hello     World   "                  =>  "#HelloWorld"
 ""                                        =>  false
 */
+// must start with #, first char of each word capitalised 
+// result longer than 140chars is false, empty string as input = false
+
+/* Plan 
+- handle fail case => empty string 
+- prep string to split into array 
+- remove whitespaces at start and end 
+- replace whitespaces in the middle 
+- /\s+/g match all occurences of whitespace , g = global match 
+- split string into array of words 
+- loop through array 
+- add hashtag to first item
+- capitalise every item 
+- join array 
+- evaluate length of string 
+- 
+*/
+
+function generateHashtag (str) {
+  
+  if (str.trim().length === 0){
+    return false; 
+  }
+  
+ let trimmedStr = str.trim(); 
+  let equallySpacedStr = trimmedStr.replace(/\s+/g," "); 
+  
+  let arrStr = equallySpacedStr.split(' '); 
+ 
+  arrStr.forEach((word, index) => {
+    let wordArr = word.split(''); 
+    let firstChar = word.charAt(0); 
+    
+    if(index === 0){
+      wordArr[0] =  "#" + firstChar.toUpperCase(); 
+    } else {
+      wordArr[0] = firstChar.toUpperCase()
+    }
+    arrStr[index] = wordArr.join(''); 
+  })
+  
+  let genHashtag = arrStr.join(''); 
+   
+  if (genHashtag.length > 140 ){
+    return false
+  } 
+  
+  return genHashtag; 
+}
