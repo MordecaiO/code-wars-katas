b513c594648456ad3511931f2cb13c4892663bd3
MordecaiO <cai.oladimeji@gmail.com>
2022-10-20 19:48:05 +0100

    Update 5kyu_Pete_the_baker.js

--- 5kyu/5kyu_Pete_the_baker.js
+++ 5kyu/5kyu_Pete_the_baker.js
@@ -13,3 +13,45 @@ cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, mil
 // must return 0
 cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 
 */
+
+/* Plan 
+- first check if we have all the availible ingredients
+- extract both object keys into seperate arrays 
+- loop through recipe arr and check if each item is present in availible array
+- if any item is not present return 0
+- loop through the recipe object 
+- divide availible obj prop value by recipe obj prop value (availible divided by recipe)
+- round down value 
+- push to an array 
+- find min value in the array 
+*/
+
+
+function cakes(recipe, available) {
+  
+  let servings = null; 
+  let recipeArr = Object.keys(recipe); 
+  let availableArr = Object.keys(available); 
+ 
+  recipeArr.forEach((item) => {
+    if(!availableArr.includes(item)){
+      servings = 0; 
+    }
+  });
+  if (servings === 0){
+    return servings
+  } else {
+     let servingsArr = [];
+  
+  for (const item in recipe){
+    let exactServings = available[item] / recipe[item]; 
+    let actualServings = Math.floor(exactServings); 
+   
+    servingsArr.push(actualServings); 
+  }
+  
+  servings = Math.min(...servingsArr); 
+  return servings
+  }
+ 
+}
