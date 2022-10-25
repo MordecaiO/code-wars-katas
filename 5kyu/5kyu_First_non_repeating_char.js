c4e441e200ce52df5c5b47c71e353dcbe081dc76
MordecaiO <cai.oladimeji@gmail.com>
2022-10-25 10:44:06 +0100

    Update 5kyu_First_non_repeating_char.js

--- 5kyu/5kyu_First_non_repeating_char.js
+++ 5kyu/5kyu_First_non_repeating_char.js
@@ -8,3 +8,40 @@ but the function should return the correct case for the initial letter. For exam
 
 If a string contains all repeating characters, it should return an empty string ("") or None -- see sample tests.
 */
+
+/* Plan 
+- check if each letter repeats one by one 
+- when checking exclude the letter itself 
+- if no repeats found not down the letter 
+- repeat 
+- now look at your list of non repeating letters 
+- take the first one and return it 
+*/
+
+
+function firstNonRepeatingLetter(s) {
+   
+  if(s.length === 1){
+    return s
+  }
+  
+  let letterArr = s.split(""); 
+  let nonRepeatingChars = []; 
+  
+  letterArr.forEach((letter, index, array) => {
+    
+    let arr1 = array.slice(0, index);
+    let arr2 = array.slice(index + 1);
+    let testArr =  [...arr1, ...arr2]
+    
+    if (testArr.length){
+      
+      if (testArr.includes(letter.toLowerCase()) === false && testArr.includes(letter.toUpperCase()) === false  ) {
+        nonRepeatingChars.push(letter);
+      }
+    }
+  })
+    
+  return nonRepeatingChars.length >= 1 ? nonRepeatingChars[0] : "" ; 
+  
+  }
