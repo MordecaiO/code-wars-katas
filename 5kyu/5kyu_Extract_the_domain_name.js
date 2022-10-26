e85c7cd0f1055bba2d9d7b86a785426f20ddb9a2
MordecaiO <cai.oladimeji@gmail.com>
2022-10-26 17:40:16 +0100

    Update 5kyu_Extract_the_domain_name.js

--- 5kyu/5kyu_Extract_the_domain_name.js
+++ 5kyu/5kyu_Extract_the_domain_name.js
@@ -5,3 +5,17 @@ Write a function that when given a URL as a string, parses out just the domain n
 * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
 * url = "https://www.cnet.com"                -> domain name = cnet"
 */
+
+
+function domainName(url){
+  
+  // replace http or https with empty string
+  url = url.replace("http://", "");
+  url = url.replace("https://", "");
+  // replace www. with empty string
+  url = url.replace("www.", "");
+  // seperate domain name with "." as seperator
+  let urlArr = url.split("."); 
+ 
+  return urlArr[0]
+}
