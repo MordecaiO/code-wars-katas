95863573be5e048df775cd7f3f06752af6f26d23
MordecaiO <cai.oladimeji@gmail.com>
2022-10-29 19:48:33 +0100

    Update 6kyu_Break_camel_case.js

--- 6kyu/6kyu_Break_camel_case.js
+++ 6kyu/6kyu_Break_camel_case.js
@@ -6,3 +6,36 @@ Example
 "identifier"   =>  "identifier"
 ""             =>  ""
  */
+/*plan
+- check each char one by one in order
+- at the first capital letter that appears in the word
+- split that word from there 
+- if no capital word found return word
+- if word is empty return empty word
+*/
+
+
+
+function solution(string) {
+  
+  if (string.length < 1) return string;
+  
+  let breakIndex = []; 
+  
+  let arrChars = string.split(''); 
+  
+  arrChars.forEach((char, index, array) => {
+   
+    if(char.toUpperCase() === char){
+     breakIndex.push(index)  
+    } 
+  })
+  
+  
+  breakIndex.forEach((x, index) => {
+    arrChars.splice(x + index, 0, ' '); 
+  })
+   
+   
+  return arrChars.join('')
+}
