8e26e06b05696de2a546be7ff08d1deb3d011377
MordecaiO <cai.oladimeji@gmail.com>
2022-10-30 18:23:37 +0000

    Create 7kyu_Categorize_new_member.js

---
+++ 7kyu/7kyu_Categorize_new_member.js
@@ -0,0 +1,13 @@
+/*
+Input
+Input will consist of a list of pairs. Each pair contains information for a single potential member. 
+Information consists of an integer for the person's age and an integer for the person's handicap.
+
+Output
+Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+
+Example
+input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+
+*/
