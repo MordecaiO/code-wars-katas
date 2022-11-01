26a7ffaef42afd5908f4ba5f624fcda1326493a0
MordecaiO <cai.oladimeji@gmail.com>
2022-11-01 21:50:34 +0000

    Update 6kyu_The_supermarket_queue.js

--- 6kyu/6kyu_The_supermarket_queue.js
+++ 6kyu/6kyu_The_supermarket_queue.js
@@ -20,3 +20,15 @@ queueTime([10,2,3,3], 2)
 queueTime([2,3,10], 2)
 // should return 12
 */
+function queueTime(customers, n) {
+  
+  let tills = new Array(n).fill(0); 
+  
+  customers.forEach((customer) => {
+    let nextTill = tills.indexOf(Math.min(...tills)); 
+    
+    tills[nextTill] += customer;
+  })
+  
+  return Math.max(...tills);
+  }
