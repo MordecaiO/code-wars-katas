c70cc8af3dd5da6fc44f4d3fe030093468feb281
MordecaiO <cai.oladimeji@gmail.com>
2022-11-09 11:30:32 +0000

    Update 6kyu_Count_chars_in_your_string.js

--- 6kyu/6kyu_Count_chars_in_your_string.js
+++ 6kyu/6kyu_Count_chars_in_your_string.js
@@ -5,3 +5,34 @@ What if the string is empty? Then the result should be empty object literal, {}.
 
 
 */
+
+
+/*Plan 
+- create return obj
+- handle empty string case 
+- walk through input string 
+- if current item isn't present is return obj
+- create property with item as key and 1 as count
+- else add one to property with cur item as key
+*/
+
+function count (string) {  
+  
+  let obj = {}; 
+  
+  if (string.length < 1) return obj; 
+  else {
+    
+    string.split("").forEach((char) =>{
+      
+      if (obj.hasOwnProperty(char) !== true){
+        obj[char] = 1 ; 
+      } else {
+        obj[char] += 1 ; 
+      }
+      
+      console.log('obj', obj); 
+    })
+  }
+   return obj ;
+}
