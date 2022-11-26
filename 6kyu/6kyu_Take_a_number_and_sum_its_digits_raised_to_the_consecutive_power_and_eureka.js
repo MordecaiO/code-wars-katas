d558850e7a404e870caf3bf37335a1608fdb51eb
MordecaiO <cai.oladimeji@gmail.com>
2022-11-26 14:03:44 +0000

    Update 6kyu_Take_a_number_and_sum_its_digits_raised_to_the_consecutive_power_and_eureka.js

--- 6kyu/6kyu_Take_a_number_and_sum_its_digits_raised_to_the_consecutive_power_and_eureka.js
+++ 6kyu/6kyu_Take_a_number_and_sum_its_digits_raised_to_the_consecutive_power_and_eureka.js
@@ -12,3 +12,44 @@ If there are no numbers of this kind in the range [a, b] the function should out
 90, 100 --> []
 */
 
+/* Plan
+- num = sum of each digit raised to consec power 
+- test every num in range except last 
+- one by one approach 
+- create somewhere to store eureka nums
+- if num has less than 2 digits return num 
+- else take num and divide into indv digits 
+- raise each indv digit to power based on order
+- if sum is = to num add to eureka nums 
+*/
+
+
+function sumDigPow(a, b) {
+ 
+  let eureka = []; 
+  
+  for (let i = a; i < b; i++){
+    
+    
+    if (i < 10){
+      eureka.push(i); 
+    } else {
+      
+    let iString = String(i); 
+    let iArr = iString.split(''); 
+    let sum = 0; 
+      
+      iArr.forEach((digit, index) => {
+      
+        sum += Math.pow(digit, index + 1)
+        
+      })
+      
+      if (sum === i ){
+        eureka.push(i); 
+      }
+    }
+  }
+  
+  return eureka;
+}
