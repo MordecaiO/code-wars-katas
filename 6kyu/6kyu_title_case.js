68eb18d6d3d2f4290926d98e55f38f62d8d81387
MordecaiO <cai.oladimeji@gmail.com>
2022-12-15 08:10:56 +0000

    Update 6kyu_title_case.js

--- 6kyu/6kyu_title_case.js
+++ 6kyu/6kyu_title_case.js
@@ -16,3 +16,34 @@ titleCase('a clash of KINGS', 'a an the of') // should return: 'A Clash of Kings
 titleCase('THE WIND IN THE WILLOWS', 'The In') // should return: 'The Wind in the Willows'
 titleCase('the quick brown fox') // should return: 'The Quick Brown Fox'
 */
+
+
+function titleCase(title, minorWords) {
+  
+  // when input is empty return empty string 
+  if (!title.length > 0 ) return "" ; 
+  
+  // store minorWords in obj as lower case 
+   let minorWordsObj = {};
+  if (minorWords){
+    minorWords.split(" ").forEach((word) => {
+    minorWordsObj[word.toLowerCase()] = 1 ; 
+  })
+  } 
+  
+  // take string and lowercase each word one by one Also capitalise first word 
+  let titleArr = title.split(" "); 
+ let titleArr2 = titleArr.map((word) => { 
+   return word[0].toUpperCase() + word.slice(1).toLowerCase(); 
+  })
+ 
+  // loop through array of words
+  // if we are not at index 0 & the word isn't present in my key capitalise it 
+  let titleArr3 = titleArr2.map((word, i) => {
+     let minorWord = minorWordsObj[word.toLowerCase()] ;
+     return i !== 0 && minorWord ? word.toLowerCase() : word
+   })
+  // return title 
+  
+  return titleArr3.join(" ");
+}
