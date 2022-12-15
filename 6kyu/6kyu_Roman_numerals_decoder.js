ce0e60d5b7982a3d260770a2cf74e269ea46e77d
MordecaiO <cai.oladimeji@gmail.com>
2022-12-15 08:08:44 +0000

    Update 6kyu_Roman_numerals_decoder.js

--- 6kyu/6kyu_Roman_numerals_decoder.js
+++ 6kyu/6kyu_Roman_numerals_decoder.js
@@ -12,3 +12,29 @@ Example:
 
 solution('XXI'); // should return 21
 */
+
+
+function solution (roman) {
+  // create var to store accumulating integer
+  let intValue = 0; 
+  // create obj with symbol & value pairs
+  let key = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000};
+  // for each char in string..
+  for (let i = 0; i < roman.length; i++){
+      // determine value and add to integer
+    let numeral = roman[i]; 
+    
+    let nextNumeral = roman[i + 1] ? roman[i + 1] : "I"
+    
+    if (key[numeral] >= key[nextNumeral]){
+      intValue += key[numeral]; 
+    } else {
+      intValue -= key[numeral]; 
+    }
+    
+    console.log('numeral', numeral); 
+    console.log('intValue', intValue); 
+  } 
+  // return integer
+	return intValue;
+}
