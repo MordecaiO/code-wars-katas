27b4ca16ded7b5890a0f46b62adf34a784f685ce
MordecaiO <cai.oladimeji@gmail.com>
2022-12-16 12:07:07 +0000

    Update 5kyu_String_incrementer.js

--- 5kyu/5kyu_String_incrementer.js
+++ 5kyu/5kyu_String_incrementer.js
@@ -20,3 +20,62 @@ Attention: If the number has leading zeros the amount of digits should be consid
 
 */
 
+
+/* 
+write a function which when given a string creates a new string where a number at the end 
+of the string is incremented by one. 
+if there is no number at the end of the string append 1 to the end of the old string to create new str
+if there are any zero's before the number at the end take all valid integers and consider them a number
+*/
+
+function incrementString (str) {
+ 
+  // determine what the string contains 
+  let alpha = false ; 
+  let num = false; 
+  let strArr = str.split("");
+  
+  strArr.forEach((x) => { 
+    if (isNaN(parseInt(x)) ){alpha = true ;} 
+    if (!isNaN(parseInt(x))){num = true ; } 
+  })
+  // if the string is all alphabetical chars 
+  if (alpha && !num) return str + 1 ; 
+  
+  // if the string is all numeric chars
+  if (!alpha && num){
+    // numeric chars with/without leading zeros 
+    const lastLeadZero = str.lastIndexOf("0"); 
+    // no leading zeros
+    if(lastLeadZero === -1){
+      return (parseInt(str) + 1).toString()
+    // leading zeros
+    } else {
+      let padLength = str.length;
+      let numToIncr = str.slice(lastLeadZero + 1);
+      numToIncr = (parseInt(numToIncr) + 1).toString()
+      return numToIncr.padStart(padLength, 0);
+    }
+  }
+  // if the string is both alphabetic and numeric
+    if (alpha && num){
+     
+        let indexFirstNumChar = str.length - str.split("").reverse().findIndex((x) => isNaN(parseInt(x)))
+        let numToIncr1 = str.slice(indexFirstNumChar);
+        const lastLeadZero1 = numToIncr1.lastIndexOf("0");
+          
+      // if end section has no leading zeros
+      if(lastLeadZero1 === -1){
+      numToIncr1 =  (parseInt(numToIncr1) + 1).toString()
+      return str.slice(0,indexFirstNumChar) + numToIncr1
+        // if end section has leading zeros
+    } else {
+      let padLength = numToIncr1.length;
+      let numToIncr = numToIncr1.slice(lastLeadZero1 + 1);
+      let numToIncr2 = (parseInt(numToIncr1) + 1).toString()
+     
+      numToIncr2 = numToIncr2.padStart(padLength, 0); 
+      return str.slice(0, indexFirstNumChar) + numToIncr2 ;
+    }
+}
+}
