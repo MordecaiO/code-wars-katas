1a2ab97ec34d82ca277f0cad1b9d98e4ae2bea6a
MordecaiO <cai.oladimeji@gmail.com>
2023-02-08 19:19:47 +0000

    Update 5kyu_Product_of_consecutive_Fib_numbers.js

--- 5kyu/5kyu_Product_of_consecutive_Fib_numbers.js
+++ 5kyu/5kyu_Product_of_consecutive_Fib_numbers.js
@@ -1,1 +1,14 @@
 
+function productFib(prod){
+  
+   const recur = (a,b)=>{
+        if (a * b > prod)
+            return [a, b, false];
+        if (a * b === prod)
+            return [a, b, true];
+        return recur(b, a + b);
+    }
+
+    return recur(0, 1);
+
+}
