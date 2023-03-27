08325726506c8c2c92e7bb988c26901ef5294b69
MordecaiO <cai.oladimeji@gmail.com>
2023-03-27 20:37:16 +0100

    Update 7kyu_reverse_words.js

--- 7kyu/7kyu_reverse_words.js
+++ 7kyu/7kyu_reverse_words.js
@@ -6,3 +6,12 @@ Examples
 "This is an example!" ==> "sihT si na !elpmaxe"
 "double  spaces"      ==> "elbuod  secaps"
 */
+function reverseWords(str) {
+  let arrRevStr = []; 
+  let arrStr = str.split(" "); 
+  arrStr.forEach((word)=> {
+    arrRevStr.push(word.split("").reverse().join(""))
+  })
+  
+  return arrRevStr.join(" "); 
+}
