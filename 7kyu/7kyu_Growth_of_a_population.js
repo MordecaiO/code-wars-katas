09d3469c673c32c67c95df50966fed8c06c850e5
MordecaiO <cai.oladimeji@gmail.com>
2023-04-18 08:53:45 +0100

    Update and rename Growth_of_a_population.js to 7kyu_Growth_of_a_population.js

--- 7kyu/7kyu_Growth_of_a_population.js
+++ 7kyu/7kyu_Growth_of_a_population.js
@@ -22,3 +22,19 @@ Examples:
 nb_year(1500, 5, 100, 5000) -> 15
 nb_year(1500000, 2.5, 10000, 2000000) -> 10*/
 
+// p0 , percent , aug , p 
+// initialise a counter to keep track of the years (iterations)
+// create a while loop 
+// while the population is lesser than the target 
+// run our formula 
+// make sure the aug deals with negative numbers 
+// return the counter
+
+function nbYear(p0, percent, aug, p) {
+    let years = 0 ; 
+  while (p > p0){
+     p0 = Math.floor(p0 + (p0 * (percent/100)) + aug) ; 
+    years++ ; 
+  }
+  return years
+}
