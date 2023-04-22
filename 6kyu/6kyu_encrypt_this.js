a33a17ca08e0e2fd7a3e5ff60a9837c20334247b
MordecaiO <cai.oladimeji@gmail.com>
2023-04-22 18:09:02 +0100

    Update 6kyu_encrypt_this.js

--- 6kyu/6kyu_encrypt_this.js
+++ 6kyu/6kyu_encrypt_this.js
@@ -12,3 +12,39 @@ encryptThis("Hello") === "72olle"
 encryptThis("good") === "103doo"
 encryptThis("hello world") === "104olle 119drlo"
 */
+
+
+/* 
+first letter of each word converted to ASCII & 2nd letter switched with last letter
+- take the text then seperate into idv words
+- switch 2nd and last letter / 
+change first letter to ascii / charCodeAt(index)
+
+
+*/
+
+var encryptThis = function(text) {
+  let encryptedTextArr = [];
+  let switchedTextArr = [];
+  let textArr=text.split(" "); 
+  
+  textArr.forEach((word,index,arr)=>{
+    if(word.length>1){
+       let wordArr=word.split("");
+    let temp = wordArr[1];
+    wordArr.splice(1,1,arr[index][arr[index].length-1] );
+    wordArr.pop();
+      wordArr.push(temp);
+    switchedTextArr.push(wordArr.join(""));
+    } else {
+       switchedTextArr.push(word);
+    }
+  });
+  
+  switchedTextArr.forEach((word,index)=>{
+    let wordArr=word.split("");
+    wordArr.splice(0,1, word.charCodeAt(0) + "")
+   encryptedTextArr.push(wordArr.join("")) ;
+  })
+  return encryptedTextArr.join(" ");
+}
