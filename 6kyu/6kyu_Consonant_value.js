22b2117b50dc3c3f4ca983c784622b22eb8646a5
MordecaiO <cai.oladimeji@gmail.com>
2023-04-30 14:33:11 +0100

    Update 6kyu_Consonant_value.js

--- 6kyu/6kyu_Consonant_value.js
+++ 6kyu/6kyu_Consonant_value.js
@@ -12,3 +12,25 @@ solve("zodiacs") = 26
 For the word "strength", solve("strength") = 57
 -- The consonant substrings are: "str" and "ngth" with values "str" = 19 +
 */
+
+function solve(s) {
+  let keyObj = {a:1,b:2,c:3,d:4,e:5,f:6,g:7,h:8,i:9,j:10,k:11,l:12,m:13
+               ,n:14,o:15,p:16,q:17,r:18,s:19,t:20,u:21,v:22,w:23,x:24,y:25,z:26}
+  
+  let vowelObj = {a:"woop",e:"woop",i:"woop",o:"woop",u:"woop"}
+  
+  let sArr = s.split("");
+  let score = 0; 
+  let maxScore = 0; 
+  
+ for (let i = 0; i < sArr.length; i++){
+   let char = sArr[i]; 
+   if (!vowelObj[char]){
+     score+= keyObj[char]; 
+   } else { 
+     score = 0
+   }
+   if (score > maxScore) maxScore = score ; 
+ }
+  return maxScore
+}
