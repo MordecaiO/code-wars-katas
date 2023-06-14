83b707a089eb5ad3c977fc9cc745eb6b06815326
MordecaiO <cai.oladimeji@gmail.com>
2023-06-14 06:01:18 +0100

    Update 6kyu_Top_10_customers_by_total_payments_amount.sql

--- 6kyu/6kyu_Top_10_customers_by_total_payments_amount.sql
+++ 6kyu/6kyu_Top_10_customers_by_total_payments_amount.sql
@@ -18,3 +18,11 @@ Database Schema
 https://www.postgresqltutorial.com/postgresql-getting-started/postgresql-sample-database/
 
 */
+
+SELECT DISTINCT c.customer_id, c.email, COUNT(p.payment_id) AS payments_count, 
+CAST(SUM(p.amount) AS FLOAT) 
+AS total_amount FROM customer c 
+JOIN payment p ON c.customer_id = p.customer_id 
+GROUP BY c.customer_id 
+ORDER BY total_amount DESC
+LIMIT 10 
