40796f0a99bc7a3521e403f8e901e31677394936
MordecaiO <cai.oladimeji@gmail.com>
2023-12-04 13:01:12 +0000

    Update 7kyu_Alphabetical_Addition.ts

--- 7kyu/7kyu_Alphabetical_Addition.ts
+++ 7kyu/7kyu_Alphabetical_Addition.ts
@@ -16,3 +16,17 @@ addLetters('z', 'a') = 'a'
 addLetters('y', 'c', 'b') = 'd' // notice the letters overflowing
 addLetters() = 'z'
 */
+
+export function addLetters(...letters: string[]) {
+  
+  if(letters.length < 1) return 'z'
+  let total = 0; 
+  letters.forEach((char)=>{
+    let charVal = char.charCodeAt(0) - 96
+    total += charVal 
+    if(total > 26) total = total - 26 
+  })
+  
+  const totalChar = String.fromCharCode(total+96)       
+  return totalChar
+}
