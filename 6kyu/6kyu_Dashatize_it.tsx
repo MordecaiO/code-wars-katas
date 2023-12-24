18cd9ad66df7fea28f62b49fbeffddff58b32658
MordecaiO <cai.oladimeji@gmail.com>
2023-12-24 15:10:10 +0000

    Update 6kyu_Dashatize_it.tsx

--- 6kyu/6kyu_Dashatize_it.tsx
+++ 6kyu/6kyu_Dashatize_it.tsx
@@ -5,3 +5,25 @@ Ex:
 274 -> '2-7-4'
 6815 -> '68-1-5'
 */
+export function dashatize(num: number) : string {
+  
+  const absNum : number = Math.abs(num)
+  const absNumStr : string = absNum.toString()
+  const numArr = absNumStr.split("")
+  
+  let dirtyNumArr = numArr.map((num,index,arr) => {
+    if(parseInt(num) % 2 === 0) {
+      return num
+    } else {
+      return `-${num}-` 
+    }
+  })
+  
+  dirtyNumArr = dirtyNumArr.join("").split("")
+  if(dirtyNumArr[0] === "-") dirtyNumArr.shift()
+  if(dirtyNumArr[dirtyNumArr.length-1] === "-") dirtyNumArr.pop()
+     const dirtyNumStr : string = dirtyNumArr.join("")
+        let cleanStr : string = dirtyNumStr.replace(/(--)/g, "-") 
+  
+  return cleanStr
+};
