098f0d24fa712f2b3969824139cc9164a007b6e9
MordecaiO <cai.oladimeji@gmail.com>
2023-12-30 23:52:14 +0000

    Update 7kyu_friend_or_foe.js

--- 7kyu/7kyu_friend_or_foe.js
+++ 7kyu/7kyu_friend_or_foe.js
@@ -4,9 +4,7 @@ Make a program that filters a list of strings and returns a list with only your 
 If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
 
 Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
-
 i.e.
-
 friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
 Note: keep the original order of the names in the output.
 */
