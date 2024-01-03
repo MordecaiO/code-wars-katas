39b7d0090daf61ae1d5fbaffef5ebc3fde95e4b2
MordecaiO <cai.oladimeji@gmail.com>
2024-01-03 04:39:54 +0000

    Update 6kyu_Length_of_missing_array.ts

--- 6kyu/6kyu_Length_of_missing_array.ts
+++ 6kyu/6kyu_Length_of_missing_array.ts
@@ -14,3 +14,7 @@ If the array of arrays is null/nil or empty, the method should return 0.
 When an array in the array is null or empty, the method should return 0 too!
 There will always be a missing element and its length will be always between the given arrays.
 */
+export function getLengthOfMissingArray(arrayOfArrays:any[]):number {
+  // your code here
+  return 0
+}
