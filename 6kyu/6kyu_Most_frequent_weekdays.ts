bd9f5ab27654d6b16e7afe42e4576e4d5759070a
MordecaiO <cai.oladimeji@gmail.com>
2024-01-06 05:46:16 +0000

    Update 6kyu_Most_frequent_weekdays.ts

--- 6kyu/6kyu_Most_frequent_weekdays.ts
+++ 6kyu/6kyu_Most_frequent_weekdays.ts
@@ -23,6 +23,36 @@ For every common year DoW start day of the year increases by 1 day, for every le
 Calculate DoW year begins on , determine whether a leap year or not and calculate how many times days are included in cycle of 52 or 53 weeks 
 */
 
-export function mostFrequentDays(year: number): string[] {
-    // Implement me! :)
+
+    export function mostFrequentDays(year: number): string[] {
+  const dayKeyObj : {[index:number]:string} = {
+    0:"Sunday",
+    1:"Monday",
+    2:"Tuesday",
+    3:"Wednesday", 
+    4:"Thursday", 
+    5:"Friday", 
+    6:"Saturday"
+  };
+  const isLeapYear = (yearAsInt: number): boolean => {
+   return ( (yearAsInt % 4 == 0 && yearAsInt % 100 != 0) || yearAsInt % 400 == 0)
+    }
+  let date : Date = new Date(`${year}-01-01`)
+  let doW : number = date.getDay()
+   let daysExtra : number = 1;  
+   let daysArr : number[] = []
+  if(isLeapYear(year)) {
+    daysArr = [doW, doW + 1]
+    if(daysArr[0] == 6) daysArr[1] = 0
+    if(daysArr[0] == 0){
+      [daysArr[0], daysArr[1]] = [daysArr[1], daysArr[0]]
+    }
+  } else {
+    daysArr = [doW]
+  }
+  let returnArr : string[] = daysArr.map((num)=>{
+    return dayKeyObj[num]
+  })
+  return returnArr
 }
+
