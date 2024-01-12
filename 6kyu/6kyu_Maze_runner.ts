bab3cb1e8ecefb761e881afee5e474aed7942a78
MordecaiO <cai.oladimeji@gmail.com>
2024-01-12 07:28:08 +0000

    Update 6kyu_Maze_runner.ts

--- 6kyu/6kyu_Maze_runner.ts
+++ 6kyu/6kyu_Maze_runner.ts
@@ -1,1 +1,24 @@
+/*
+Introduction
+Welcome Adventurer. Your aim is to navigate the maze and reach the finish point without touching any walls. Doing so will kill you instantly!
+Task
+You will be given a 2D array of the maze and an array of directions. Your task is to follow the directions given.
+If you reach the end point before all your moves have gone, you should return Finish. If you hit any walls or go outside the maze border, 
+you should return Dead. If you find yourself still in the maze after using all the moves, you should return Lost.
+The Maze array will look like
 
+maze = [[1,1,1,1,1,1,1],
+        [1,0,0,0,0,0,3],
+        [1,0,1,0,1,0,1],
+        [0,0,1,0,0,0,1],
+        [1,0,1,0,1,0,1],
+        [1,0,0,0,0,0,1],
+        [1,2,1,0,1,0,1]]
+..with the following key
+
+      0 = Safe place to walk
+      1 = Wall
+      2 = Start Point
+      3 = Finish Point
+  direction = {"N","N","N","N","N","E","E","E","E","E"} == "Finish"
+*/
