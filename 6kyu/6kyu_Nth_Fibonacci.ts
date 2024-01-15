18fc865a102a6cdc918b34ffc191da369131d66c
MordecaiO <cai.oladimeji@gmail.com>
2024-01-15 04:45:01 +0000

    Update 6kyu_Nth_Fibonacci.ts

--- 6kyu/6kyu_Nth_Fibonacci.ts
+++ 6kyu/6kyu_Nth_Fibonacci.ts
@@ -11,3 +11,7 @@ Because 2 is the 4th number in the Fibonacci Sequence.
 For reference, the first two numbers in the Fibonacci sequence are 0 and 1, and each subsequent number is the sum of the previous two.
 
 */
+export function nthFibo(n: number): number {
+
+ return 0
+}
