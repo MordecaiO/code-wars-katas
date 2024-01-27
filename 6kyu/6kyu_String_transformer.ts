ef49417ba7108ae33d94cf77a6b6a623ece42cf5
MordecaiO <cai.oladimeji@gmail.com>
2024-01-27 12:25:04 +0000

    Update 6kyu_String_transformer.ts

--- 6kyu/6kyu_String_transformer.ts
+++ 6kyu/6kyu_String_transformer.ts
@@ -10,5 +10,27 @@ For example:
 You may assume the input only contain English alphabet and spaces.*/
 
 export function stringTransformer(str: string) {
-return ""
+// change chars
+  let strArr : string[] = str.split("")
+    let transformedStrArr = strArr.map((char)=>{
+      if(char == " "){
+      return char
+    } else if(char == char.toUpperCase()){
+      return char.toLowerCase()
+    } else if (char == char.toLowerCase()){
+      return char.toUpperCase()
+    }
+  })
+    // find all words and whitespaces 
+    let returnArr : string[] =[]
+    let regEx = /[\w]+|[\s]+/g ;
+    let wordMatches : RegExpMatchArray | null = transformedStrArr.join("").match(regEx)
+    // list words and spaces in reverse order
+  if(wordMatches){
+     for(let i=wordMatches.length-1; i>=0; i--){
+      returnArr.push(wordMatches[i])
+    }
+  }
+  return str == "" ? "" : returnArr.join("")
+}
 }
