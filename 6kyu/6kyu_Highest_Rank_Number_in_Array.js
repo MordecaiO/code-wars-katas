04ede96feeb2b97555727b77e03e36ca6f4d20f4
MordecaiO <cai.oladimeji@gmail.com>
2024-07-06 10:27:36 +0100

    Update 6kyu_Highest_Rank_Number_in_Array.js

--- 6kyu/6kyu_Highest_Rank_Number_in_Array.js
+++ 6kyu/6kyu_Highest_Rank_Number_in_Array.js
@@ -9,3 +9,18 @@ Examples
 [12, 10, 8, 12, 7, 6, 4, 10, 12, 10]          -->  12
 [12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]  -->   3
 */
+
+function highestRank(arr){
+let countObj = {}
+arr.forEach((num)=> countObj[num] ? countObj[num] += 1: countObj[num] = 1)
+  let newArr = Object.keys(countObj)
+    .map((el) => Number(el))
+      .sort((a,b)=> b-a)
+  let currLargest = newArr[0] ; 
+  newArr.forEach((num) => {
+    let numCount = countObj[num]
+    let currLargestNumCount = countObj[currLargest]
+    if(numCount > currLargestNumCount) currLargest = num 
+  })
+ return currLargest
+}
