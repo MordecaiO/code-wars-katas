f424eb3cf06e1ef537686c79db6732f5431a02d5
MordecaiO <cai.oladimeji@gmail.com>
2024-08-18 11:15:12 +0100

    Update 5kyu_Mean_Square_Error.py

--- 5kyu/5kyu_Mean_Square_Error.py
+++ 5kyu/5kyu_Mean_Square_Error.py
@@ -8,3 +8,12 @@ Examples
 [10, 20, 10, 2], [10, 25, 5, -2]  -->  16.5 because (0 + 25 + 25 + 16) / 4
 [-1, 0], [0, -1]                  -->   1   because (1 + 1) / 2
 '''
+import math
+def solution(array_a, array_b):
+    sum_squared_diffs = 0
+    for index in range(len(array_a)): 
+        array_a_val = array_a[index]
+        array_b_val = array_b[index]
+        squared_diff = math.pow(abs(array_a_val - array_b_val),2)
+        sum_squared_diffs += squared_diff
+    return sum_squared_diffs / len(array_a)
