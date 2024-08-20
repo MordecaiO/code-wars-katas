18a8cc7eb0792ee7ca4b53dfa47adfc5cbe51e80
MordecaiO <cai.oladimeji@gmail.com>
2024-08-20 06:20:28 +0100

    Update 7kyu_Some_but_not_all.py

--- 7kyu/7kyu_Some_but_not_all.py
+++ 7kyu/7kyu_Some_but_not_all.py
@@ -1,1 +1,14 @@
+'''
+Your task is to create a function that given a sequence and a predicate, 
+returns True if only some (but not all) the elements in the sequence are True after applying the predicate
+Examples
+('abcdefg&%$', x -> isLetter(x)) == true
+('&%$=', x -> isLetter x) == false
+('abcdefg', x -> isLetter x) == false
+
+([4, 1], x -> x > 3) == true
+([1, 1], x -> x > 3) == false
+([4, 4], x -> x > 3) == false
+'''
+
 
