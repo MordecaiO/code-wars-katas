7b7abfc20918ac311a2fa2c89fb28ea870504076
MordecaiO <cai.oladimeji@gmail.com>
2024-08-22 06:35:49 +0100

    Update 6kyu_Deaf_Rats_of_Hamelin.py

--- 6kyu/6kyu_Deaf_Rats_of_Hamelin.py
+++ 6kyu/6kyu_Deaf_Rats_of_Hamelin.py
@@ -17,3 +17,26 @@ ex2 P O~ O~ ~O O~ has 1 deaf rat
 
 ex3 ~O~O~O~OP~O~OO~ has 2 deaf rats
 '''
+def count_deaf_rats(town):
+    found_piper = False
+    deaf_rats = 0
+    expected_char = '~' 
+    index=0
+    while(index < len(town)):
+        print(town[index])
+        
+        if town[index] == 'P':
+            expected_char = 'O'
+            index += 1 
+            
+        elif town[index] == ' ':
+            index += 1
+            
+        elif town[index] == expected_char:
+            index +=2
+            
+        else: 
+            deaf_rats += 1
+            index += 2
+    return deaf_rats
+    pass
