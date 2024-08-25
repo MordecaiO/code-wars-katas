1a7fae8b7b22f3fbce3491e7043c551922804d88
MordecaiO <cai.oladimeji@gmail.com>
2024-08-25 11:17:07 +0100

    Update 7kyu_Maximum_Multiple.py

--- 7kyu/7kyu_Maximum_Multiple.py
+++ 7kyu/7kyu_Maximum_Multiple.py
@@ -1,1 +1,10 @@
+'''
+Given a Divisor and a Bound , Find the largest integer N , Such That ,
 
+Conditions :
+N is divisible by divisor
+
+N is less than or equal to bound
+
+N is greater than 0.
+'''
