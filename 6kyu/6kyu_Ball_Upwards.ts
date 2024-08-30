f323c03a1dc934d6114e7dd979ce9641df336ece
MordecaiO <cai.oladimeji@gmail.com>
2024-08-30 06:49:25 +0100

    Update 6kyu_Ball_Upwards.ts

--- 6kyu/6kyu_Ball_Upwards.ts
+++ 6kyu/6kyu_Ball_Upwards.ts
@@ -12,3 +12,20 @@ max_ball(15) should return 4
 
 max_ball(25) should return 7
 */
+
+export function maxBall(v0: number): number {
+  let currMaxHeight : number = 0;
+  let maxStep : number = 0; 
+  for(let i=0; i<=v0; i++){
+    let timeInSecs : number = i == 0 ? i : i/10
+    let velocityMS : number = (v0*1000)/3600
+    let height : number = velocityMS*timeInSecs - (0.5*9.81*timeInSecs*timeInSecs)
+    if(height >= currMaxHeight){
+      currMaxHeight = height
+    } else {
+      maxStep = i-1
+      break
+    }
+  }
+  return maxStep
+}
