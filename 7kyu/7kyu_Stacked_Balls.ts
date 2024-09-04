439cf968f875f0a0dd2693e095e52d754de92fda
MordecaiO <cai.oladimeji@gmail.com>
2024-09-04 06:21:35 +0100

    Update 7kyu_Stacked_Balls.ts

--- 7kyu/7kyu_Stacked_Balls.ts
+++ 7kyu/7kyu_Stacked_Balls.ts
@@ -13,3 +13,9 @@ Return the height as multiple of the ball diameter.
 Example
 The image above shows a stack of 5 layers.
 */
+
+export function stackHeight2d(layers: number) {
+  if (layers == 0 || layers == 1) return layers
+ let h : number = Math.sqrt(Math.pow(layers-1,2) - Math.pow(0.5*(layers-1),2)) +1
+  return h
+}
