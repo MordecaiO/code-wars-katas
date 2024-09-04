6a10802cc641fc2522cab2e2a17e561d84b4bcd6
MordecaiO <cai.oladimeji@gmail.com>
2024-09-04 06:21:10 +0100

    Update 7kyu_Stacked_Balls.ts

--- 7kyu/7kyu_Stacked_Balls.ts
+++ 7kyu/7kyu_Stacked_Balls.ts
@@ -1,1 +1,15 @@
+/*
+Background
+I have stacked some pool balls in a triangle.
 
+Like this,
+
+pool balls
+Kata Task
+Given the number of layers of my stack, what is the total height?
+
+Return the height as multiple of the ball diameter.
+
+Example
+The image above shows a stack of 5 layers.
+*/
