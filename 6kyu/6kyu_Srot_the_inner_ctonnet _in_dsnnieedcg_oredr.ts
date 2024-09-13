50b007ed0633069e3d6c6559503c5585d225c104
MordecaiO <cai.oladimeji@gmail.com>
2024-09-13 05:56:23 +0100

    Update 6kyu_Srot_the_inner_ctonnet _in_dsnnieedcg_oredr.ts

--- 6kyu/6kyu_Srot_the_inner_ctonnet _in_dsnnieedcg_oredr.ts
+++ 6kyu/6kyu_Srot_the_inner_ctonnet _in_dsnnieedcg_oredr.ts
@@ -15,6 +15,20 @@ The string will never be null and will never be empty. In C/C++ the string is al
 Have fun coding it and please don't forget to vote and rank this kata! :-)
 
 I have also created other katas. Take a look if you enjoyed this kata!
-
-
 */
+
+export function sortTheInnerContent(words:string):string
+{
+  const wordsArr: string[] = words.split(" ")
+   let returnStr = wordsArr.map((word) => {
+     if(word.length <= 3){
+       return word
+     } else {
+       const firstChar: string = word[0] , lastChar: string = word[word.length-1] ;
+       let innerContent : string[] = word.slice(1,word.length-1).split("")
+       let sortedInnerContent : string = innerContent.sort().reverse().join("")
+       return firstChar+sortedInnerContent+lastChar
+     }
+   }).join(" ")
+  return returnStr;
+}
