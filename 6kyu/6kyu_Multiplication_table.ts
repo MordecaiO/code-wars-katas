8b873cd2c5c46617a963f68f8ae80143bb2bd152
MordecaiO <cai.oladimeji@gmail.com>
2024-09-14 09:39:01 +0100

    Update 6kyu_Multiplication_table.ts

--- 6kyu/6kyu_Multiplication_table.ts
+++ 6kyu/6kyu_Multiplication_table.ts
@@ -10,3 +10,15 @@ For the given example, the return value should be:
 
 [[1,2,3],[2,4,6],[3,6,9]]
 */
+
+export function multiplicationTable (size: number): number[][] {
+  let table : number[][] = []
+  for(let i=0; i<size; i++){
+    let row = new Array(size).fill(1)
+    for(let k=0; k<size; k++){
+      row[k] = (i+1)*(k+1)
+    }
+    table.push(row)
+  }
+  return table
+}
