702e2aaee146569a39c7559cbcf4db45497ddc29
MordecaiO <cai.oladimeji@gmail.com>
2024-09-16 05:20:10 +0100

    Update 7kyu_Sort_Numbers.ts

--- 7kyu/7kyu_Sort_Numbers.ts
+++ 7kyu/7kyu_Sort_Numbers.ts
@@ -5,3 +5,7 @@ For example:
 solution([1, 2, 10, 50, 5]); // should return [1, 2, 5, 10, 50]
 solution([]); // should return []
 */
+
+export function solution(nums: number[]): number[] {
+  return nums.length > 1 ? nums.sort((a,b)=>a-b) : []; 
+}
