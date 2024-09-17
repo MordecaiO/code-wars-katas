b3079ba0e0b2a92894f307e1a2d413018040e134
MordecaiO <cai.oladimeji@gmail.com>
2024-09-17 08:49:48 +0100

    Update 7kyu_Closure_Counter.ts

--- 7kyu/7kyu_Closure_Counter.ts
+++ 7kyu/7kyu_Closure_Counter.ts
@@ -1,1 +1,17 @@
+/*
+Define the function counter that returns a function that returns an increasing value.
+The first value should be 1.
+You're going to have to use closures.
+Example:
+const newCounter = counter();
+newCounter() // 1
+newCounter() // 2
 
+*/
+export function counter(): Function {
+  let i =1 
+  const count = (input:number) => {
+    return i++
+  }
+  return count
+};
