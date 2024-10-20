24cf80e9f1d6b0d3d06d187b4384ffd029b2ad6a
MordecaiO <cai.oladimeji@gmail.com>
2024-10-20 15:55:04 +0100

    Update 7kyu_Longest_file_chain.ts

--- 7kyu/7kyu_Longest_file_chain.ts
+++ 7kyu/7kyu_Longest_file_chain.ts
@@ -3,3 +3,17 @@ The vowel substrings in the word codewarriors are o,e,a,io. The longest of these
 Given a lowercase string that has alphabetic characters only (both vowels and consonants) and no spaces,
 return the length of the longest vowel substring. Vowels are any of aeiou.
 */
+
+
+export function solve(s: string) {
+  const vowels : string[] = ["a","e","i","o","u"];
+  let count = 0; 
+  for(let i=0; i<=s.length; i++){
+    let j = 0; 
+    while(vowels.includes(s[j+i])){
+      j++ 
+      if(j > count) count = j 
+    }
+  }
+  return count;
+}
