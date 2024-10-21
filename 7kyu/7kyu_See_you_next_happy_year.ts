b2de5515bad08dbd1bd36a17ec326af13c93cecc
MordecaiO <cai.oladimeji@gmail.com>
2024-10-21 06:31:57 +0100

    Update 7kyu_See_you_next_happy_year.ts

--- 7kyu/7kyu_See_you_next_happy_year.ts
+++ 7kyu/7kyu_See_you_next_happy_year.ts
@@ -1,1 +1,8 @@
+/*
+You're saying good-bye your best friend , See you next happy year .
 
+Happy Year is the year with only distinct digits , (e.g) 2018
+
+Task
+Given a year, Find The next happy year or The closest year You'll see your best friend
+*/
